71d6ab1b605baee8a737b1fe005578aaa35629e0
august0-m <152826406+august0-m@users.noreply.github.com>
2024-06-14 18:36:16 -0300

    update clase materia

--- Trabajo Practico 2_v2 (1)/estudiantes/src/main/java/aed/Materia.java
+++ Trabajo Practico 2_v2 (1)/estudiantes/src/main/java/aed/Materia.java
@@ -2,6 +2,44 @@ package aed;
 
 public class Materia {
     private Tripla<ListaEnlazada<String>,int[],Carrera> datosXmateria;
-    
+
+    public Materia(){
+        ListaEnlazada<String> estudiantes = new ListaEnlazada<String>();
+        int[] docentes = new int[3];
+        Carrera refACarrera = null;
+
+        datosXmateria = new Tripla<ListaEnlazada<String>,int[],Carrera>(estudiantes, docentes, refACarrera);
+    }
+    public void agregarAlumno(String alumno){
+        ListaEnlazada<String> alumnos = datosXmateria.getPrimero();
+
+        alumnos.agregarAtras(alumno);
+    }
+    public int cantidadAlumnos(){
+        ListaEnlazada<String> alumnos = datosXmateria.getPrimero();
+
+        return alumnos.longitud();
+    }
+    public void agregarDocente(int docente){
+        int[] docentes = datosXmateria.getSegundo();
+        if(docente == 0){
+           docentes[0] ++;
+        }
+        if(docente == 1){
+            docentes[1] ++;
+         }
+         if(docente == 2){
+            docentes[2] ++;
+         }
+         if(docente ==3){
+            docentes[3] ++;
+         }
+        return;
+    }
+
+    public int[] getDocente(){
+        int[] docentes = datosXmateria.getSegundo();
+        return docentes;
+    }
+
 }
-//
