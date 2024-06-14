11df8bbc99b7d85016eea9c656cc5322e052f8e0
Sebastian <soutosebastianma@gmail.com>
2024-06-14 18:41:08 -0300

    funciones carrera

--- Trabajo Practico 2_v2 (1)/estudiantes/src/main/java/aed/Carrera.java
+++ Trabajo Practico 2_v2 (1)/estudiantes/src/main/java/aed/Carrera.java
@@ -2,6 +2,52 @@ package aed;
 
 public class Carrera {
     private Trie<Materia> materias;
-     
     
+    public Carrera(){
+
+        materias = new Trie<Materia>();
+    } 
+
+    public void agregarMateria(String materia, Materia datosMateria){
+
+        materias.insertar(materia,datosMateria);
+    }
+
+
+    public void agregarAlumnoCarrera(String materia, String estudiante){
+        
+        Materia datoMateria = materias.obtener(materia);
+
+        datoMateria.agregarAlumno(estudiante);
+    }
+
+
+    public int cantidadInscriptosCarrera(String materia){
+
+        Materia datoMateria = materias.obtener(materia);
+
+        int res = datoMateria.cantidadAlumnos();
+
+        return res;
+    }
+
+
+    public void agregarDocenteCarrera(String materia, int docente){
+
+        Materia datoMateria = materias.obtener(materia);
+
+        datoMateria.agregarDocente(docente);
+    }
+
+
+    public int[] plantelDocenteCarrera(String materia){
+
+        Materia datoMateria = materias.obtener(materia);
+
+        int[] res = datoMateria.getDocente();
+
+        return res;
+    }
+
+
 }
